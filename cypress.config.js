c6ac1afc666cc49b8ad5d782ed4489f9430f0d65
RalphWangoto <wangotoraphael@gmail.com>
2025-07-29 16:08:54 +0300

    Implemented CI GitHub Actions Pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
+  reporter: require.resolve('mochawesome'), // Explicit path
   reporterOptions: {
-      reportDir: 'cypress/reports',
-      overwrite: false,
-      html: true,
-      json: true,
-      charts: true,
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      timestamp: 'mmddyyyy_HHMMss',
-      reportFilename: 'cypress-report',
-      pageTitle: 'Cypress Test Report',
-      reportPageTitle: 'Cypress Test Report',
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    timestamp: 'mmddyyyy_HHMMss'
   },
   e2e: {
     testIsolation: false,
     defaultCommandTimeout: 25000,
     viewportWidth: 1440,
     viewportHeight: 800,
-
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      require('cypress-mochawesome-reporter/plugin')(on);
-    },
-    
-    },
-  });
+      //require('cypress-mochawesome-reporter/plugin')(on);
+
+      return config;
+    }
+  }
+});
